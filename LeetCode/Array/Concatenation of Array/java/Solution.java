4bfd66527d89a8fff47e7bd7f9fa0ccee139b420
Gowthamarajan Pommusamy <alonergowtha@gmail.com>
2026-08-21 15:22:06 +0530

    feat: Add Concatenation of Array

--- LeetCode/Array/Concatenation of Array/java/Solution.java
+++ LeetCode/Array/Concatenation of Array/java/Solution.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int n=nums.length;
-        List <Integer> list=new ArrayList<>();
-        for(int i:nums){
-            list.add(i);
-        }
-            list.addAll(list);
-            int arr[]=new int[list.size()];
-            int ind=0;
-            for(int i:list){
-                 arr[ind++]=i;
-            }
-           return arr;
-        
-    }     
+     int[] nums1= new int[nums.length];
+      for(int i = 0;i<nums1.length;i++){
+          nums1[i] = nums[i];
+      } 
+      int[]res = new int[nums.length+nums1.length]; 
+   System.arraycopy(nums,0,res,0,nums.length);
+   System.arraycopy(nums1,0,res,nums.length,nums1.length);
+    return res;
+    }
 }
