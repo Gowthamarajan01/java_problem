7ae04dca73f05c672203db81f5bb3a376d7e0fe8
Gowthamarajan Pommusamy <alonergowtha@gmail.com>
2026-08-29 19:55:47 +0530

    feat: Add Reverse Words in a String

--- LeetCode/Two Pointers/Reverse Words in a String/java/Solution.java
+++ LeetCode/Two Pointers/Reverse Words in a String/java/Solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words=s.trim().split("\\s+");
-        StringBuilder ans=new StringBuilder();
-        for(int i=words.length-1;i>=0;i--){
-            ans.append(words[i]).append(" ");
+        String[] str = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for(int i =str.length-1;i>=0;i--){
+             sb.append(str[i]);
+             if(i!=0) sb.append(" ");
         }
-        return ans.toString().trim();
+        return sb.toString().trim();
     }
 }
